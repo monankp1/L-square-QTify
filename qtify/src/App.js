15d8c0f460c4f4871d17f6b7115f98610aefe224
monankp1 <monankp1@gmail.com>
2023-11-29 13:30:31 +0530

    Commit done code updated

---
+++ qtify/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { StyledEngineProvider } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { fetchNewAlbums, fetchTopAlbums } from './api/api';
+
+function App() {
+  const [data, setData] = useState({});
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        return { ...prevState, [key]: data };
+      })
+    })
+  }
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+  }, []);
+  const { topAlbums = [], newAlbums = []} = data;
+  console.log(data);
+
+  return (
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar/>
+        <Outlet context={{data: {topAlbums, newAlbums}}} />
+      </StyledEngineProvider>
+    </>
+  );
+}
+
+export default App;
